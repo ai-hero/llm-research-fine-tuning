b55342311b75c8b9b2760463030b70cef33a7b5e
Rahul Parundekar <rparundekar@gmail.com>
2024-02-17 21:30:54 -0800

    Switch to pydantic

--- launch.py
+++ launch.py
@@ -1,5 +1,5 @@
 """run script for fine-tuning a model."""
-from aihero.research.config import BatchInferenceJobConfig, TrainJobConfig
+from aihero.research.config.schema import BatchInferenceJob, TrainingJob
 from aihero.research.finetuning.infer import BatchInferenceJobRunner
 from aihero.research.finetuning.train import TrainingJobRunner
 from fire import Fire
@@ -7,13 +7,13 @@ from fire import Fire
 
 def train(training_config_file: str) -> None:
     """Run Training."""
-    training_config = TrainJobConfig.load(training_config_file)
+    training_config = TrainingJob.load(training_config_file)
     TrainingJobRunner(training_config).run()
 
 
 def infer(batch_inference_config_file: str) -> None:
     """Run Batch Inference."""
-    batch_inference_config = BatchInferenceJobConfig.load(batch_inference_config_file)
+    batch_inference_config = BatchInferenceJob.load(batch_inference_config_file)
     BatchInferenceJobRunner(batch_inference_config).run()
 
 
