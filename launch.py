51240466089d47b9015c05662328e314a6332c4c
Rahul Parundekar <rparundekar@gmail.com>
2024-05-05 09:37:39 -0700

    load distributed config

--- launch.py
+++ launch.py
@@ -1,14 +1,20 @@
 """run script for fine-tuning a model."""
+import yaml  # type: ignore
 from aihero.research.config.schema import BatchInferenceJob, TrainingJob
 from aihero.research.finetuning.infer import BatchInferenceJobRunner
 from aihero.research.finetuning.train import TrainingJobRunner
 from fire import Fire
 
 
-def train(training_config_file: str = "/mnt/config/training/config.yaml") -> None:
+def train(training_config_file: str = "/mnt/config/training/config.yaml", distributed_config_file: str = "") -> None:
     """Run Training."""
     training_config = TrainingJob.load(training_config_file)
-    TrainingJobRunner(training_config).run()
+    if distributed_config_file:
+        with open(distributed_config_file) as f:
+            distributed_training_config = yaml.safe_load(f)
+    else:
+        distributed_training_config = ""
+    TrainingJobRunner(training_config, distributed_training_config=distributed_training_config).run()
 
 
 def infer(batch_inference_config_file: str = "/mnt/config/batch_inference/config.yaml") -> None:
